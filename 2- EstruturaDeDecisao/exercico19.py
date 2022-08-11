a2d9b520b2c47719afe349868f519604aa4ad510
Eliezer Silva <eliezersilva.dev@gmail.com>
2022-08-11 00:28:45 -0300

    addExercicios

--- 2- EstruturaDeDecisao/exercico19.py
+++ 2- EstruturaDeDecisao/exercico19.py
@@ -9,12 +9,38 @@ Observando os termos no plural a colocação do "e", da vírgula entre outros. E
 Testar com: 326, 300, 100, 320, 310,305, 301, 101, 311, 111, 25, 20, 10, 21, 11, 1, 7 e 16
 """
 
-numero = 326
+print()
+print('-'*10 + ' Vericador de Centenas, Dezenas e Unidades '+'-'*10)
+print()
+
+numero = int(input('Digite um número entre 1 e 999: '))
 
 centena = int(numero / 100)
 dezena = int((numero - (centena * 100)) / 10)
 unidade = int(numero - (centena * 100 + dezena * 10))
 
-print(centena)
-print(dezena)
-print(unidade)
+string_centena = 'centena'
+string_dezena = 'dezena'
+string_unidade = 'unidade'
+
+if centena > 1:
+    string_centena = 'centenas'
+if dezena > 1:
+    string_dezena = 'dezenas'
+if unidade > 1:
+    string_unidade = 'unidades'
+
+resultado = (f'{numero}: = {centena} {string_centena}, {dezena} {string_dezena} e {unidade} {string_unidade}')
+
+if centena == 0:
+    resultado = (f'{numero}: = {dezena} {string_dezena} e {unidade} {string_unidade}')
+if dezena == 0:
+    resultado = (f'{numero}: = {centena} {string_centena} e {unidade} {string_unidade}')
+if unidade == 0:
+    resultado = (f'{numero}: = {centena} {string_centena} e {dezena} {string_dezena}')
+else:
+    print(resultado)
+
+
+
+
