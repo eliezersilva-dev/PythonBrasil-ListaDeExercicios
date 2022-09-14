081dc30458f37e0416c1c054e9768a4342a0a436
Eliezer Silva <eliezersilva.dev@gmail.com>
2022-09-13 22:16:23 -0300

    addExercicio

---
+++ 3 - Estrutura de Repetição 5-51/exercicio_006.py
@@ -0,0 +1,14 @@
+"""
+Eliezer Silva
+Exercício 06
+_____________
+
+Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro.
+Depois modifique o programa para que ele mostre os números um ao lado do outro.
+"""
+
+lista = list(range(1, 21, 1))
+for i in lista:
+    print(i)
+print()
+print(lista)
