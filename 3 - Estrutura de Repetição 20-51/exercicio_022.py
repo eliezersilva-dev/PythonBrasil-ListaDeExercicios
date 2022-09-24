cb8299ebd717ba691228c92f16019019b57c5f72
Eliezer Silva <eliezersilva.dev@gmail.com>
2022-09-23 23:14:29 -0300

    addExercicio

--- 3 - Estrutura de Repetição 20-51/exercicio_022.py
+++ 3 - Estrutura de Repetição 20-51/exercicio_022.py
@@ -6,14 +6,11 @@ Altere o programa de cálculo dos números primos, informando,
 caso o número não seja primo, por quais número ele é divisível.
 """
 
-numero = int(input("Digite um numero inteiro: "))
+numero = int(input('Digite um numero inteiro: '))
 primo = True
 for i in range(2, numero):
     if numero % i == 0:
         primo = False
-        print(f"{numero} não é primo! É divisível por {i}.")
+        print(f'{numero} não é primo! É divisível por {i}.')
 if primo:
-    print(f"{numero} é primo!")
-
-
-
+    print(f'{numero} é primo!')
