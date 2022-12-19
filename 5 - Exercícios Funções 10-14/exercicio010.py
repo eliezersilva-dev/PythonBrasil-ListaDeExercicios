38d9ef02dc05283934a33cb54aa185f34c2b3f24
Eliezer Silva <eliezersilva.dev@gmail.com>
2022-12-19 14:17:57 -0300

    addExercicio

--- 5 - Exercícios Funções 10-14/exercicio010.py
+++ 5 - Exercícios Funções 10-14/exercicio010.py
@@ -11,21 +11,74 @@ tirar um 7 antes de tirar este Ponto novamente.
 import random
 import time
 
+print()
+
+contador = 0
+pontos_jogador = 0
+
+
+def placar(pontos):
+    global pontos_jogador
+
+    if contador == 1:
+        pontos_jogador = pontos
+        if pontos_jogador == 7 or pontos_jogador == 11:
+            print('Natural!! Você ganhou!!')
+            quit()
+        elif pontos_jogador == 2 or pontos_jogador == 3 or pontos_jogador == 12:
+            print('Craps!! Você perdeu!!')
+            quit()
+        else:
+            print(f'Seu ponto é {pontos_jogador}')
+        rodada()
+    else:
+        print(f'Seu ponto: {pontos_jogador}')
+        if pontos_jogador == pontos:
+            print('Você tirou seu ponto!! Você ganhou!!')
+            quit()
+        elif pontos == 7:
+            print('Você tirou 7!! Você perdeu :(')
+            quit()
+        else:
+            print('Você não tirou seu ponto. Próxima rodada...')
+            rodada()
+
 
 def jogar_dados():
     print()
     print('Jogando os dados...')
-    time.sleep(3)
+    time.sleep(1.7)
     dado_1 = random.randint(1, 6)
     print(f'Dado 1 = {dado_1}')
-    time.sleep(3)
+    time.sleep(1.7)
     dado_2 = random.randint(1, 6)
     print(f'Dado 2 = {dado_2}')
     time.sleep(1.5)
     pontos = dado_1 + dado_2
-    print(f'Pontos = {pontos}')
-    
-
-jogar_dados()
+    print(f'Pontos da rodada = {pontos}')
+    placar(pontos)
 
 
+def rodada():
+    global contador
+
+    if contador == 0:
+        print('Vamos começar o jogo!')
+    else:
+        print(f'Próxima rodada.')
+
+    print()
+    jogar = input('Jogar dados? [s/n]: ')
+
+    if jogar == 's':
+        pass
+    else:
+        print('Saindo...')
+        time.sleep(1.5)
+        quit()
+
+    contador += 1
+    jogar_dados()
+
+
+rodada()
