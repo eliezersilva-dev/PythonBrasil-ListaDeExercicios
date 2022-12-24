e95389091cfcbb3007a2d3ce6b640beb3bf3d00f
Eliezer Silva <eliezersilva.dev@gmail.com>
2022-12-23 21:03:07 -0300

    addExercício

--- 5 - Exercícios Funções 14-14/exercicio014.py
+++ 5 - Exercícios Funções 14-14/exercicio014.py
@@ -13,38 +13,25 @@ Elabore uma função que identifica e mostra na tela todos os quadrados mágicos
 Dica: produza todas as combinações possíveis e verifique a soma quando completar cada quadrado.
 Usar um vetor de 1 a 9 parece ser mais simples que usar uma matriz 3x3.
 """
+from itertools import permutations
+print()
 
 
-# Não terminei ainda :(
-import itertools
-from itertools import combinations, permutations
+def valida_tabela(tabela):
+    if sum(tabela[:3]) == sum(tabela[3:6]) == sum(tabela[6:10]):
+        if sum(tabela[::3]) == sum(tabela[1::3]) == sum(tabela[2::3]):
+            if sum(tabela[::4]) == sum(tabela[2:8:2]):
+                print('Quadrado válido:')
+                print(f'{tabela[:3]}\n'
+                      f'{tabela[3:6]}\n'
+                      f'{tabela[6:10]}')
+                print()
 
 
-# def valida_tabela(tabela): # Verifica se a tabela é válida
-#     if sum(tabela[:3]) == sum(tabela[3:6]) == sum(tabela[6:10]):
-#         if sum(tabela[::3]) == sum(tabela[1::3]) == sum(tabela[2::3]):
-#             if sum(tabela[::4]) == sum(tabela[2:8:2]):
-#                 print(tabela, ' valida ')
-#                 return 1
-#            else: return 0
-#        else: return 0
-#    else:
-#        return 0
-#
-#
-# def gera(tab): #gerar as matrizes possíveis
-#     cont = 0
-#     validos = 0
-#     for i in permutations(tab,9):
-#         cont += 1
-#         validos += validaTabela(i)
-#    print(f'total de verificações {cont} e matriz válidas {validos}')
-#
-# tabela = [1,2,3,4,5,6,7,8,9]
-#
-# gera(tabela)
+def gera_tabela(tabela):
+    for i in permutations(tabela, 9):
+        valida_tabela(i)
 
 
-
-for i in itertools.count():
-    print(i)
+tabela = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+gera_tabela(tabela)
